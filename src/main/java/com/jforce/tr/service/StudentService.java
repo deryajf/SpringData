dd15648a02eed51d54d3bc49856d6d2a53817392
LAPTOP-I4K2NJ8E\Derya Bektaş <derya.bektas@jforce.com>
2022-04-07 15:28:02 +0300

    SpringData Örnek

---
+++ src/main/java/com/jforce/tr/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.jforce.tr.service;
+
+import java.util.List;
+
+import com.jforce.tr.modal.Student;
+
+public interface StudentService {
+	List<Student> getList();
+
+	Student getStudentById(Integer id);
+
+	void deleteStudentById(Integer id);
+
+	void addStudent(Student student);
+	
+	void updateStudent(Student student);
+}
